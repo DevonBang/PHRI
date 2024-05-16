be22d874fe903532a86e17dd978dc789b71bb637
DevonBang <163628404+DevonBang@users.noreply.github.com>
2024-05-16 15:26:56 +0700

    reset

--- resources/views/detail-berita.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('template.main')
-@section('content')
-    <h1>halaman detail berita</h1>
-    <a href="{{ route('berita') }}">Back to berita</a>
-        <h1>title: {{ $berita->title }}</h1>
-        <p> {!! $berita->description !!}</p>
-        <img src="{{ asset('storage/berita-images/' . $berita->image) }}">
-@endsection
